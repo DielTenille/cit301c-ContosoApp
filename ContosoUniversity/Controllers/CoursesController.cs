dc00ca74f61f933994f7921e0b36c2bedd8097e3
DielTenille <die12010@byui.edu>
2017-03-20 21:27:26 -0600

    Completed all pages of the tutorial.  Ready to be turned in.

--- ContosoUniversity/Controllers/CoursesController.cs
+++ ContosoUniversity/Controllers/CoursesController.cs
@@ -131,7 +131,28 @@ namespace ContosoUniversity.Controllers
                                    select d;
             ViewBag.DepartmentID = new SelectList(departmentsQuery.AsNoTracking(), "DepartmentID", "Name", selectedDepartment);
         }
-        
+
+        //GET: UpdateCourseCredits
+        public IActionResult UpdateCourseCredits()
+        {
+            return View();
+        }
+
+        //POST: UpdateCourseCredits
+        [HttpPost]
+        public async Task<IActionResult> UpdateCourseCredits(int? multiplier)
+        {
+            if (multiplier != null)
+            {
+                ViewData["RowsAffected"] =
+                    await _context.Database.ExecuteSqlCommandAsync(
+                        "UPDATE Course SET Credits = Credits * {0}",
+                        parameters: multiplier);
+            }
+            return View();
+        }
+
+
 
         // GET: Courses/Delete/5
         public async Task<IActionResult> Delete(int? id)
